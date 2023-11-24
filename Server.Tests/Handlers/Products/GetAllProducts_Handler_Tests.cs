5c716432731bf888c30fa2f3d6fbbee91be2ff29
alexgoold <alexander.goold@iths.se>
2023-11-24 10:35:57 +0100

    red

--- Server.Tests/Handlers/Products/GetAllProducts_Handler_Tests.cs
+++ Server.Tests/Handlers/Products/GetAllProducts_Handler_Tests.cs
@@ -1,8 +1,10 @@
 ﻿using Application.UnitOfWork;
 using AutoMapper;
 using FakeItEasy;
-using Server.Endpoints.Products.Get;
 using Server.Endpoints.Products.GetAll;
+using Shared.ProductsDtos;
+using Tests.Helpers;
+using Xunit;
 
 namespace Tests.Handlers.Products;
 
@@ -20,6 +22,21 @@ public class GetAllProducts_Handler_Tests
 		_dummyRequest = A.Dummy<GetAllProductsRequest>();
 		_dummyRequest.UnitOfWork = _fakeUnitOfWork;
 		_sut = new GetAllProductsHandler(_fakeMapper.FakedObject);
+
+	}
+
+	[Fact]
+	public async Task GetAllProductHandler_WhenCalled_WithProductsInDb_Should_Return_AListOf_ProductDtos()
+	{
+		// Arrange
+		var products = ProductGenerator.GenerateListOf3Products();
+		A.CallTo(() => _fakeUnitOfWork.Products.GetAllAsync()).Returns(products);
+
+		// Act
+		var result = await _sut.Handle(_dummyRequest, CancellationToken.None);
+
+		// Assert
+		result.Should().BeOfType<List<ProductDto>>();
 
 	}
 }
