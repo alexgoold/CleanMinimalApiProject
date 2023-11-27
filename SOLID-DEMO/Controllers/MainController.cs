3ef5a5f177a8171fc15b298e53b05c24e0b707ac
alexgoold <alexander.goold@iths.se>
2023-11-27 20:35:19 +0100

    delete controller endpoints that no longer are used

--- SOLID-DEMO/Controllers/MainController.cs
+++ SOLID-DEMO/Controllers/MainController.cs
@@ -61,53 +61,7 @@ public class MainController : ControllerBase
         await _shopContext.SaveChangesAsync();
         return Ok();
     }
-
-
-    [HttpPost("/orders")]
-    public async Task<IActionResult> PlaceOrder(CustomerCart cart)
-    {
-        var customer = await _shopContext.Customers.FirstOrDefaultAsync(c => c.Id.Equals(cart.CustomerId));
-        if (customer is null)
-        {
-            return BadRequest();
-        }
-
-        var products = new List<Product>();
-
-        foreach (var prodId in cart.ProductIds)
-        {
-            var prod = await _shopContext.Products.FirstOrDefaultAsync(p => p.Id == prodId);
-            if (prod is null)
-            {
-                return NotFound();
-            }
-            products.Add(prod);
-        }
-
-        var order = new Order() { Customer = customer, Products = products };
-        var now = DateTime.Now;
-        order.ShippingDate = now.AddDays(5);
-
-        await _shopContext.Orders.AddAsync(order);
-        await _shopContext.SaveChangesAsync();
-
-        return Ok();
-    }
-
-    [HttpDelete("/orders/{id}")]
-    public async Task<IActionResult> CancelOrder(Guid id)
-    {
-        var order = await _shopContext.Orders.FirstOrDefaultAsync(o => o.Id == id);
-        if (order is null)
-        {
-            return NotFound();
-        }
-
-        _shopContext.Orders.Remove(order);
-        await _shopContext.SaveChangesAsync();
-        return Ok();
-    }
-
+    
     [HttpPatch("order/add/{id}")]
     public async Task<IActionResult> AddToOrder(CustomerCart itemsToAdd, Guid id)
     {
