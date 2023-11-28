cc126f0b799215726e283d86f4f720d1dcd31842
alexgoold <alexander.goold@iths.se>
2023-11-28 15:22:07 +0100

    remove Authenticate methods in mediatr extensions

--- SOLID-DEMO/Extensions/WeApplicationMediatrEndpointExtensions.cs
+++ SOLID-DEMO/Extensions/WeApplicationMediatrEndpointExtensions.cs
@@ -11,51 +11,22 @@ namespace Server.Extensions
                 async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request));
             return app;
         }
-
-        public static WebApplication MediateAuthenticateGet<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
-        {
-            app.MapGet(url,
-                async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request)).RequireAuthorization("admin");
-            return app;
-        }
-
         public static WebApplication MediatePost<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
         {
             app.MapPost(url,
                 async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request));
             return app;
         }
-        public static WebApplication MediateAuthenticatePost<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
-        {
-            app.MapPost(url,
-                async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request)).RequireAuthorization("admin");
-            return app;
-        }
-
         public static WebApplication MediateDelete<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
         {
             app.MapDelete(url,
                 async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request));
             return app;
         }
-
-        public static WebApplication MediateAuthenticateDelete<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
-        {
-            app.MapDelete(url,
-                async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request)).RequireAuthorization("admin");
-            return app;
-        }
-
         public static WebApplication MediatePut<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
         {
             app.MapPut(url,
                 async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request));
-            return app;
-        }
-        public static WebApplication MediateAuthenticatePut<TRequest>(this WebApplication app, string url) where TRequest : IHttpRequest
-        {
-            app.MapPut(url,
-                async (IMediator mediator, [AsParameters] TRequest request) => await mediator.Send(request)).RequireAuthorization("admin");
             return app;
         }
     }
---
+++ Server.Tests/Handlers/Orders/AddToOrder_Handler_Tests.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Tests.Handlers.Orders;
+
+public class AddToOrder_Handler_Tests
+{
+
+
+
+}
