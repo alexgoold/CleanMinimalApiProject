2907bd6e62f2fc7a8b611a7af38873073265fd50
alexgoold <alexander.goold@iths.se>
2023-11-30 17:17:06 +0100

    begin login tdd

---
+++ Server.Tests/Handlers/Customers/LoginCustomer_Handler_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Application.UnitOfWork;
+using AutoMapper;
+using FakeItEasy;
+using Xunit;
+
+namespace Tests.Handlers.Customers;
+
+public class LoginCustomer_Handler_Tests
+{
+	private readonly LoginCustomerHandler _sut;
+	private readonly IUnitOfWork _fakeUnitOfWork;
+	private readonly LoginCustomerRequest _dummyRequest;
+
+	public LoginCustomer_Handler_Tests()
+	{
+		_fakeUnitOfWork = A.Fake<IUnitOfWork>();
+		_dummyRequest = A.Dummy<LoginCustomerRequest>();
+		_dummyRequest.UnitOfWork = _fakeUnitOfWork;
+		_sut = new LoginCustomerHandler(_fakeMapper.FakedObject);
+
+	}
+
+	[Fact]
+	public void Handle_WhenGivenValid_Email_And_CorrectPassword_ShouldReturn_Ok()
+	{
+
+	}
+
+	
+}
